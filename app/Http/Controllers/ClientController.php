ff35175d472208c6a1889e1eacd24ae2a1a952f7
guicunha <guilherme@alugoaki.com.br>
2015-12-04 02:39:09 -0200

    put

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -21,16 +21,6 @@ class ClientController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -53,26 +43,15 @@ class ClientController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
-     */
+     */ 
     public function update(Request $request, $id)
     {
-        //
+        \CodeProject\Client::find($id)->update($request->all());
     }
 
     /**
